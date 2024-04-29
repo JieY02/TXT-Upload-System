78b310f1fabc40f4562fe0b8ec0e3ae3c9bc0744
JieY02 <1264065719@qq.com>
2024-04-29 23:41:05 +0800

    Feature: Implement the basic frame of Server

--- cwk/server/Server.java
+++ cwk/server/Server.java
@@ -1,7 +1,58 @@
-public class Server
-{
-    public static void main(String[] args)
-    {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class Server {
+    private static final String ERROR_LOG_FILE_NOT_CREATE = "Can't create a new log file.";
+
+    private static final int PORT = 9876;
+
+    public static void main(String[] args) throws IOException {
+        logInit();
+
+        ExecutorService executor = Executors.newFixedThreadPool(20);
+
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        System.out.println("Server is running on port " + PORT);
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
+            new Thread(() -> handleClient(clientSocket)).start();
+        }
+    }
+
+    private static void handleClient(Socket clientSocket) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             BufferedWriter writer = new BufferedWriter(new FileWriter("received_file.txt"))) {
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void logInit() {
+        File logFile = new File("log.txt");
+
+        if (logFile.exists()) {
+            logFile.delete();
+        }
+
+        try {
+            if (!logFile.createNewFile()) {
+                System.err.println(ERROR_LOG_FILE_NOT_CREATE);
+                return;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
